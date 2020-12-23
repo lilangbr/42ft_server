54a5f76247e65b69707c4be5f07fb9ad37922e97
lilangbr <eblang@gmail.com>
2020-12-22 22:35:03 -0300

    Testing DB connection from PHP

--- srcs/config.sh
+++ srcs/config.sh
@@ -25,10 +25,10 @@ chmod 666 /etc/ssl/certs/nginx-selfsigned.key /etc/ssl/certs/nginx-selfsigned.cr
 
 ##_____ STEP 3 - MySQL
 # Create a database and a user test
-#service mysql start
-#echo "CREATE DATABASE exampledb;" | mysql -u root
-#echo "GRANT ALL ON exampledb.* TO 'exampleuser'@'localhost' IDENTIFIED BY 'exampleuser' WITH GRANT OPTION;" | mysql -u root
-#echo "FLUSH PRIVILEGES;" | mysql -u root
+service mysql start
+echo "CREATE DATABASE exampledb;" | mysql -u root
+echo "GRANT ALL ON exampledb.* TO 'exampleuser'@'localhost' IDENTIFIED BY 'exampleuser' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
 # ***** All Right! *****
 
 
@@ -37,6 +37,14 @@ chmod 666 /etc/ssl/certs/nginx-selfsigned.key /etc/ssl/certs/nginx-selfsigned.cr
 chown -R www-data:www-data /var/www/*
 #chmod -R 755 /var/www/*
 
-# info.php to test
-# Send certificates to snippets folder
+# Send info.php to test
 cp /tmp/to_varwww/info.php /var/www/html/info.php
+
+##_____STEP 4_1 - Testing Database Connection from PHP
+# Send todo_list.php, a file to test
+cp /tmp/to_varwww/todo_list.php /var/www/html/todo_list.php
+echo "CREATE TABLE exampledb.todo_list (item_id INT AUTO_INCREMENT,content VARCHAR(255),PRIMARY KEY(item_id));" | mysql -u root
+echo "INSERT INTO exampledb.todo_list (content) VALUES ('My first important item');" | mysql -u root
+echo "INSERT INTO exampledb.todo_list (content) VALUES ('My second important item');" | mysql -u root
+echo "INSERT INTO exampledb.todo_list (content) VALUES ('My third important item');" | mysql -u root
+echo "SELECT * FROM exampledb.todo_list;" | mysql -u root > /teste4_1.txt
