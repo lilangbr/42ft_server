2373db7db8c18f2dbb55f2c8b26d6ffb5125ac35
lilangbr <eblang@gmail.com>
2021-02-21 21:40:09 +0100

    Notes to admins

--- srcs/init.sh
+++ srcs/init.sh
@@ -19,6 +19,11 @@ Welcome to
 service nginx start 
 service mysql restart #Jah havia iniciado em STEP3 ./srcs/config
 service php7.3-fpm start
+# Warning to admin
+echo "********************************************************************"
+echo "Note1: To admin WordPress by phpMyAdmin, use 'wp_admin' as user and password."
+echo "Note2: To manage autoindex control, see root's container" 
+echo "********************************************************************"
 # Terminal mode
 /bin/bash
 
